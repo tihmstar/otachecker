93769bde0a8993d8bbacdd86a21fead16a992c5b
tihmstar <tihmstar@gmail.com>
2015-10-21 20:29:19 +0200

    Initial Commit

---
+++ otachecker/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  otachecker
+//
+//  Created by tihmstar on 21.10.15.
+//  Copyright © 2015 tihmstar. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
